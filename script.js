9700b7ecb32f1def401a80f577b889f1eb19653b
kevin <yanchenkai@gmail.com>
2022-08-09 15:41:40 -0400

    added button that erases and creates a new grid
    button prompts user for dimension, deletes the old grid using jQuery,
    and creates a new one with the specified dimension. When the mouse passover
    a div, its color is changed.

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 const container = document.querySelector(".container");
-for(let i = 0; i< 16; i++){
-    for(let j = 0; j < 16; j++){
-        const div = document.createElement("div");
-        div.classList.add('box');
-        container.appendChild(div);
-    }
-    
-}
-const divs = document.querySelectorAll(".box")
 
-divs.forEach((box) => {
-    box.addEventListener('mouseover', () => {
-        box.classList.add('light');
+function createGrid(num){ //takes dimension, creates grid and adds lighting effect
+    for(let i = 0; i< num; i++){
+        for(let j = 0; j < num; j++){
+            const div = document.createElement("div");
+            div.classList.add("box");
+            let dim = 1200/num;
+            div.style.height= `${dim}px`;
+            div.style.width= `${dim}px`;
+            container.appendChild(div);
+        }
+        
+    }
+    const divs = document.querySelectorAll(".box")
+
+    divs.forEach((box) => {
+        box.addEventListener('mouseover', () => {
+            box.classList.add('light');
+        })
     })
-})
+}
+
+
+
+
+function buttonClick(){ //input
+    jQuery('.container').empty();
+    let num = window.prompt("Enter the number of squares per side", 16);
+    if(num < 100){
+        createGrid(num);
+    }
+}
