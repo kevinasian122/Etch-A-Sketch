faa72623939c1c0fa8a3dd47f5a44636fc3629b5
kevin <yanchenkai@gmail.com>
2022-08-09 15:44:30 -0400

    fixed issue with dimension and border so the divs fit inside the border

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function createGrid(num){ //takes dimension, creates grid and adds lighting effe
         for(let j = 0; j < num; j++){
             const div = document.createElement("div");
             div.classList.add("box");
-            let dim = 1200/num;
+            let dim = 1100/num;
             div.style.height= `${dim}px`;
             div.style.width= `${dim}px`;
             container.appendChild(div);
